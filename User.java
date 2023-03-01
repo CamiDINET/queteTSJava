26b2b737ec2574baabe93354a7550b555ba7d28a
CamiDINET <117636454+CamiDINET@users.noreply.github.com>
2023-03-01 19:29:49 +0100

    Update User.java

--- User.java
+++ User.java
@@ -5,4 +5,12 @@ public class User {
    private int size;
    private Photo photos;
    private Address address; 
+
+   public User(Date birthdate, int size, Photo photos, Address address){
+
+    this.birthdate = birthdate;
+    this.size = size;
+    this.photos = photos;
+    this.address = address;
+   }
 }
