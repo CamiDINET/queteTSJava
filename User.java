26275c277ea8f18ec468c9f2ff699c937e9dceb7
CamiDINET <117636454+CamiDINET@users.noreply.github.com>
2023-03-02 10:31:01 +0100

    Update User.java

--- User.java
+++ User.java
@@ -3,10 +3,10 @@ import java.util.Date;
 public class User {
    private Date birthdate;
    private int size;
-   private Photo photos;
+   private Photo[] photos;
    private Address address; 
 
-   public User(Date birthdate, int size, Photo photos, Address address){
+   public User(Date birthdate, int size, Photo[] photos, Address address){
 
     this.birthdate = birthdate;
     this.size = size;
